93443814c21f6a3b6b2a7f70f2519d700ff147df
Franklin <franklinclark@gmail.com>
2013-12-03 23:19:28 -0800

    Updating tests to fix build

--- test/test-creation.js
+++ test/test-creation.js
@@ -5,14 +5,14 @@ var path    = require('path');
 var helpers = require('yeoman-generator').test;
 
 
-describe('tumblr generator', function () {
+describe('tumblr-theme generator', function () {
     beforeEach(function (done) {
         helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
             if (err) {
                 return done(err);
             }
 
-            this.app = helpers.createGenerator('tumblr:app', [
+            this.app = helpers.createGenerator('tumblr-theme:app', [
                 '../../app'
             ]);
             done();
